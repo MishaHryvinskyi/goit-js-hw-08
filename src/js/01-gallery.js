87d712ff38399b1aa43d480e5be7ec9cf7ace64c
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-04-02 01:50:35 +0300

    init simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+const cardItem = createListElement(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', cardItem);
+
+function createListElement(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img 
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            data-source="${original}"
+            />
+        </a>
+    </li>`;
+}).join(' ');
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
