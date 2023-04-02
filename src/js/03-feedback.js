4f3c257d271915727fd7bac7bf544b41be68749d
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-04-02 22:21:12 +0300

    finish task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
 import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+const LOCALSTORAGE_KEY =  'feedback-form-state';
+
+form.addEventListener('input', throttle(evt => {
+    const objectToSave = { email: email.value, message: message.value };
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(objectToSave));
+}, 500)
+);
+
+form.addEventListener('submit', evt => {
+    evt.preventDefault();
+    if(email.value === '' || message.value === '') {
+        return alert ('Pleace fill in all fields')
+    }
+
+    console.log({ email: email.value, message: message.value });
+    form.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+});
+
+const load = key => {
+    try {
+        const state = localStorage.getItem(key);
+        return state === null ? undefined : JSON.parse(state);
+    } catch (error) {
+        console.error('Get state error:', error.message);
+    }
+};
+
+const storageData = load(LOCALSTORAGE_KEY);
+if (storageData) {
+    email.value = storageData.email;
+    message.value = storageData.message;
+}
